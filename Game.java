24ea8ee88109fa60f8a62d7d0c3aefb52fabca15
Taha Zoabi <zoabi03@gmail.com>
2024-07-15 11:02:09 +0300

    added multiple round and score counter
    added a a multiple round game instead of ending the game after one round then added a score showcase so user can see the score of the rounds

--- Game.java
+++ Game.java
@@ -1,71 +1,104 @@
 import java.util.Scanner;
 
 public class Game {
+    private static final Scanner input = new Scanner(System.in);
+    private static int playerScore = 0;
+    private static int computerScore = 0;
+
+    public static void main(String[] args) {
+        playGame();
+    }
+
     public static void playGame() {
-        Scanner input = new Scanner(System.in);
-        System.out.println(">>Let's play Rock Paper Scissors.\n" + "Are you ready? (yes/no)");
+        System.out.println(">> Let's play Rock Paper Scissors.\nAre you ready? (yes/no)");
         String checkReady = input.nextLine();
 
-
         if (checkReady.equalsIgnoreCase("yes")) {
-            System.out.println("Great! pick your choice \nrock - paper - scissors");
-            String playerMove = input.nextLine();
-
-            String computerChoice = computerChoice();
-            String yourChoice = playerChoice(playerMove);
-
-
-            System.out.println("\nYou chose: " + yourChoice);
-            System.out.println("The computer chose: " + computerChoice);
-
-
-            String result = displayResults(yourChoice, computerChoice);
-            System.out.println(result);
-
+            playRound();
         }
         else if (checkReady.equalsIgnoreCase("no")) {
             System.out.println("We will be waiting for you xD");
         }
         else {
-            System.out.println("Invalid Input please try again!");
+            System.out.println("Invalid input, please try again!");
+            playGame();
         }
     }
 
+    public static void playRound() {
+        System.out.println("Great! Pick your choice: rock - paper - scissors");
+        String playerMove = playerChoice();
+
+        String computerMove = computerChoice();
+        System.out.println("\nYou chose: " + playerMove);
+        System.out.println("The computer chose: " + computerMove);
+
+        String result = displayResults(playerMove, computerMove);
+        System.out.println(result);
+
+
+        if (result.equals("You Win")) {
+            playerScore++;
+        }
+        else if (result.equals("You Lose")) {
+            computerScore++;
+        }
+
+
+        System.out.println("Score - You: " + playerScore + " Computer: " + computerScore);
+
+        System.out.println("Do you want to play again? (yes/no)");
+        String playAgain = input.nextLine();
+        if (playAgain.equalsIgnoreCase("yes")) {
+            playRound();
+        }
+        else {
+            System.out.println("Thank you for playing!");
+        }
+    }
+
+    public static String playerChoice() {
+        while (true) {
+            String move = input.nextLine().toLowerCase().trim();
+            if (move.equals("rock") || move.equals("paper") || move.equals("scissors")) {
+                return move;
+            }
+            else {
+                System.out.println("Invalid input. Please choose again (rock, paper, scissors):");
+            }
+        }
+    }
 
     public static String computerChoice() {
         int randomNumber = (int) (Math.random() * 3);
 
-        return switch (randomNumber) {
-            case 0 -> "rock";
-            case 1 -> "paper";
-            case 2 -> "scissors";
-            default -> throw new IllegalStateException("Unexpected value: " + randomNumber);
-        };
-    }
-
-
-    public static String playerChoice(String move) {
-        move = move.toLowerCase().trim();
-
-        if (!(move.equals("rock") || move.equals("paper") || move.equals("scissors"))) {
-            System.out.println("Invalid Input please choose a move (rock, paper, scissors");
+        switch (randomNumber) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2:
+                return "scissors";
+            default:
+                throw new IllegalStateException("Unexpected value: " + randomNumber);
         }
-        return move;
     }
 
-
-    public static String displayResults(String playMove, String compMove) {
-
-        if (playMove.equals(compMove)) {
+    public static String displayResults(String playerMove, String computerMove) {
+        if (playerMove.equals(computerMove)) {
             return "Draw!";
         }
         else {
-            return switch (compMove) {
-                case "rock" -> (playMove.equals("paper")) ? "You Win" : "You Lose";
-                case "paper" -> (playMove.equals("scissors")) ? "You Win" : "You Lose";
-                case "scissors" -> (playMove.equals("rock")) ? "You Win" : "You Lose";
-                default -> throw new IllegalStateException("Unexpected Error");
-            };
+            switch (computerMove) {
+                case "rock":
+                    return (playerMove.equals("paper")) ? "You Win" : "You Lose";
+                case "paper":
+                    return (playerMove.equals("scissors")) ? "You Win" : "You Lose";
+                case "scissors":
+                    return (playerMove.equals("rock")) ? "You Win" : "You Lose";
+                default:
+                    throw new IllegalStateException("Unexpected Error");
+            }
         }
     }
 }
